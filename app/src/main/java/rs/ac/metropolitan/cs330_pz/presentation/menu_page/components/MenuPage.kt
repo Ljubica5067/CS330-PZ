239afdf165b68a40a515630a540822ead237b606
Ljubica5067 <ljubica.djukic.5067@metropolitan.ac.rs>
2024-06-16 02:35:20 +0200

    back dugme u Menu

--- app/src/main/java/rs/ac/metropolitan/cs330_pz/presentation/menu_page/components/MenuPage.kt
+++ app/src/main/java/rs/ac/metropolitan/cs330_pz/presentation/menu_page/components/MenuPage.kt
@@ -9,8 +9,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -28,7 +32,11 @@ fun MenuPage(navController: NavController)
 {
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text("Collections") })
+            TopAppBar(navigationIcon = {
+                IconButton(onClick = {navController.popBackStack()}) {
+                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back")
+                }
+            },title = { Text("Collections") })
         }
     ) {
         Column(
